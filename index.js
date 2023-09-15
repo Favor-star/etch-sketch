c82a9dbfaaf0f06a0c24b8b4b3fcb5a1c3a5112e
favor-star <favoureliab@gmail.com>
2023-09-16 00:06:34 +0200

    Added Color applies only when clicked + hovered functionality

--- index.js
+++ index.js
@@ -1,136 +1,132 @@
-'use strict'
+"use strict";
 
-const container = document.querySelector('.main-container');
-container.setAttribute('style', 
-`display: flex;
+const container = document.querySelector(".main-container");
+container.setAttribute(
+  "style",
+  `display: flex;
 box-sizing: border-box;
 width: 500px;
 height: 500px;
 flex-direction: column; 
  border: 3px solid black;
- border-radius: 8px`);
+ border-radius: 8px`
+);
 
 var gridSize = 20;
 function getdata() {
-  const pxData = document.getElementById('textbox-data').value;
-  const newPxData = pxData.slice(0,2);
-  if(pxData == []){
-    alert('Nothing Inserted');
-  }
-  else {
+  const pxData = document.getElementById("textbox-data").value;
+  const newPxData = pxData.slice(0, 2);
+  if (pxData == []) {
+    alert("Nothing Inserted");
+  } else {
     //alert(newPxData);
     gridSize = Number(newPxData);
-  document.getElementById('textbox-data').value = null;
-  return gridSize;
-}
-}
-
-function getDataKeyboard(event){
-  const keyData = document.getElementById('textbox-data').value;
-  const newKeyData = keyData.slice(0,2);
-  if(event.keyCode === 13){
-    if(keyData == []){
-    alert('Nothing Inserted');
-  }
-  else {
-    //alert(newKeyData);
-    gridSize = Number(newKeyData);
-    alert(gridSize);
-    document.getElementById('textbox-data').value = null;
+    document.getElementById("textbox-data").value = null;
     return gridSize;
   }
-  }
-
 }
 
-
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    // row.classList.add('drawPad');
-    row.setAttribute('style', 'flex: 1; display: flex; flex-direction: row;  ');//border: 1px solid black;
-    container.appendChild(row);
-  
-    for (let j = 0; j < gridSize; j++) {
-      const column = document.createElement('div');
-      column.classList.add('column');
-    //   column.classList.add('drawPad');
-    column.addEventListener('mouseover', draw);
-    //   column.setAttribute('onmouseover', 'this.style.backgroundColor= "darkblue"' ); 
-      column.setAttribute('style', 'flex: 1; ');
-      row.appendChild(column);
+function getDataKeyboard(event) {
+  const keyData = document.getElementById("textbox-data").value;
+  const newKeyData = keyData.slice(0, 2);
+  if (event.keyCode === 13) {
+    if (keyData == []) {
+      alert("Nothing Inserted");
+    } else {
+      //alert(newKeyData);
+      gridSize = Number(newKeyData);
+      alert(gridSize);
+      document.getElementById("textbox-data").value = null;
+      return gridSize;
     }
   }
-  let clickFlag;
-function draw(){
-    this.style.backgroundColor = '#606C5D'; 
-    
 }
- 
-const buttons = document.querySelector('.buttons');
-const eraseBtn = document.createElement('button');
- eraseBtn.addEventListener('click', clearDiv);
- eraseBtn.textContent = "Erase" ;
- buttons.appendChild(eraseBtn);
- eraseBtn.setAttribute('class', 'rsBtn');
- 
 
- function clearDiv() {
-    
-    let clearedDiv = document.querySelector('.column');
-  
-    clearedDiv.addEventListener('click', function(cleared){
-    
-    cleared.style.backgroundColor = 'white';
-    });
-    
- }
+for (let i = 0; i < gridSize; i++) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  // row.classList.add('drawPad');
+  row.setAttribute("style", "flex: 1; display: flex; flex-direction: row;  "); //border: 1px solid black;
+  container.appendChild(row);
 
-
-  const resetButton = document.createElement('button');
-  resetButton.setAttribute('class', 'rsBtn');
-  resetButton.textContent = 'Reset';
-  resetButton.setAttribute('onclick', 'resetDivColor()');
-  buttons.appendChild(resetButton);
-
-  function resetDivColor() {
-    let divs = document.querySelectorAll('.column');
-    divs.forEach(function(div){
-        div.style.backgroundColor= 'white';
-    })
+  for (let j = 0; j < gridSize; j++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    //   column.classList.add('drawPad');
+    column.addEventListener("mouseover", draw);
+    //   column.setAttribute('onmouseover', 'this.style.backgroundColor= "darkblue"' );
+    column.setAttribute("style", "flex: 1; ");
+    row.appendChild(column);
   }
- const increasePxDiv = document.createElement('div');
- increasePxDiv.setAttribute('class', 'divNmbr');
- buttons.appendChild(increasePxDiv);
-  
- const pxTextbox = document.querySelector('.divNmbr');
- const pxInstr= document.createElement('p');
- pxInstr.textContent = 'Enter Grid number:';
- pxInstr.setAttribute('style', 
- `margin-top: 20px;
+}
+let clickFlag;
+function draw() {
+  this.style.backgroundColor = "#606C5D";
+}
+
+const buttons = document.querySelector(".buttons");
+const eraseBtn = document.createElement("button");
+eraseBtn.addEventListener("click", clearDiv);
+eraseBtn.textContent = "Erase";
+buttons.appendChild(eraseBtn);
+eraseBtn.setAttribute("class", "rsBtn");
+
+function clearDiv() {
+  let clearedDiv = document.querySelector(".column");
+
+  clearedDiv.addEventListener("click", function (cleared) {
+    cleared.style.backgroundColor = "white";
+  });
+}
+
+const resetButton = document.createElement("button");
+resetButton.setAttribute("class", "rsBtn");
+resetButton.textContent = "Reset";
+resetButton.setAttribute("onclick", "resetDivColor()");
+buttons.appendChild(resetButton);
+
+function resetDivColor() {
+  let divs = document.querySelectorAll(".column");
+  divs.forEach(function (div) {
+    div.style.backgroundColor = "white";
+  });
+}
+const increasePxDiv = document.createElement("div");
+increasePxDiv.setAttribute("class", "divNmbr");
+buttons.appendChild(increasePxDiv);
+
+const pxTextbox = document.querySelector(".divNmbr");
+const pxInstr = document.createElement("p");
+pxInstr.textContent = "Enter Grid number:";
+pxInstr.setAttribute(
+  "style",
+  `margin-top: 20px;
  margin-bottom: 0;
  color: darkred;
- font-size: .8rem;`);
- pxTextbox.appendChild(pxInstr)
+ font-size: .8rem;`
+);
+pxTextbox.appendChild(pxInstr);
 
- const pxText = document.createElement('input');
- pxText.setAttribute('type', 'textbox');
- pxText.setAttribute('class', ' rsBtn');
- pxText.addEventListener('keypress',getDataKeyboard);
- pxText.setAttribute('id', 'textbox-data');
- pxText.setAttribute('placeholder', ' 16 SQ ');
- pxText.setAttribute('style', `
+const pxText = document.createElement("input");
+pxText.setAttribute("type", "textbox");
+pxText.setAttribute("class", " rsBtn");
+pxText.addEventListener("keypress", getDataKeyboard);
+pxText.setAttribute("id", "textbox-data");
+pxText.setAttribute("placeholder", " 16 SQ ");
+pxText.setAttribute(
+  "style",
+  `
  background-color: white;
   margin-top: 1px; 
   margin-bottom: 0px;
   color: black;
-  font-size: .7rem;`)
- pxTextbox.appendChild(pxText);
+  font-size: .7rem;`
+);
+pxTextbox.appendChild(pxText);
 
- const pxSubmit = document.createElement('button');
- pxSubmit.textContent = 'Submit';
- pxSubmit.setAttribute('class', 'rsBtn');
- pxSubmit.setAttribute('style', 'margin-top: 5px;');
- pxSubmit.addEventListener('click', getdata);
- pxTextbox.appendChild(pxSubmit);
-
+const pxSubmit = document.createElement("button");
+pxSubmit.textContent = "Submit";
+pxSubmit.setAttribute("class", "rsBtn");
+pxSubmit.setAttribute("style", "margin-top: 5px;");
+pxSubmit.addEventListener("click", getdata);
+pxTextbox.appendChild(pxSubmit);
--- script.js
+++ script.js
@@ -1,139 +1,152 @@
-'use strict';
+"use strict";
 
 //inupt buton creations
-let buttonsDiv = document.querySelector('.buttons2');
-let resizeBox = document.createElement('input');
-resizeBox.setAttribute('id', 'inputBox');
-resizeBox.setAttribute('placeholder', '16-100 SQ');
-resizeBox.addEventListener('keydown', function(event){
-    if(event.key === 'Enter'){
-        getData();
-        submitData();
-        }
+let isclicked = false;
+let buttonsDiv = document.querySelector(".buttons2");
+let resizeBox = document.createElement("input");
+resizeBox.setAttribute("id", "inputBox");
+resizeBox.setAttribute("placeholder", "16-100 SQ");
+resizeBox.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    getData();
+    submitData();
+  }
 });
-resizeBox.setAttribute('title', 'input');
-resizeBox.setAttribute('type', 'text');
+resizeBox.setAttribute("title", "input");
+resizeBox.setAttribute("type", "text");
 buttonsDiv.appendChild(resizeBox);
 
 //Submit button creation
-let submitBtn = document.createElement('button');
-submitBtn.addEventListener('click', submitData);
-submitBtn.setAttribute('id', 'submitBtn');
-submitBtn.setAttribute('type', 'submit');
-submitBtn.textContent = 'Submit';
+let submitBtn = document.createElement("button");
+submitBtn.addEventListener("click", submitData);
+submitBtn.setAttribute("id", "submitBtn");
+submitBtn.setAttribute("type", "submit");
+submitBtn.textContent = "Submit";
 buttonsDiv.appendChild(submitBtn);
 
 //function to clear div so as not to add divs to other after clearing.
-let mainContainer2 = document.querySelector('.main-container2');
+let mainContainer2 = document.querySelector(".main-container2");
 function clearGrid() {
-    while (mainContainer2.firstChild) {
-        mainContainer2.removeChild(mainContainer2.firstChild);
-    }
+  while (mainContainer2.firstChild) {
+    mainContainer2.removeChild(mainContainer2.firstChild);
+  }
 }
-// function to create divs after taking certian size of divs 
+// function to create divs after taking certian size of divs
 function container(size) {
- clearGrid();
-    for (let i = 0; i < size; i++) {
-        let rows = document.createElement('div');
-        rows.setAttribute('class', 'rows');
-        let mainContainer2 = document.querySelector('.main-container2');
-        mainContainer2.appendChild(rows);
+  clearGrid();
+  for (let i = 0; i < size; i++) {
+    let rows = document.createElement("div");
+    rows.setAttribute("class", "rows");
+    let mainContainer2 = document.querySelector(".main-container2");
+    mainContainer2.appendChild(rows);
 
-        for (let j = 0; j < size; j++) {
-            let columns = document.createElement('div');
-            columns.setAttribute('class', 'columns');
-            columns.addEventListener('mouseover', drawColor);
-            rows.appendChild(columns);
-        }
+    for (let j = 0; j < size; j++) {
+      let columns = document.createElement("div");
+      columns.setAttribute("class", "columns");
+      columns.addEventListener("mousedown", () => {
+        isclicked = true;
+      });
+        columns.addEventListener("mousemove", (e) => {
+            if (!isclicked) return;
+        drawColor(e.target);
+      });
+        columns.addEventListener("mouseup", () => {
+            isclicked = false;
+      });
+      rows.appendChild(columns);
     }
+  }
 }
 
 container(16);
 
 //function to get user's desired div numbers
 function getData() {
-    const userInput = document.getElementById('inputBox').value;
-    return userInput;
-
+  const userInput = document.getElementById("inputBox").value;
+  return userInput;
 }
 //function to change div number after taking user's desired divs
 function submitData() {
-    let resized = getData();
-    if (resized >= 16 && resized <= 100) {
-        container(resized);
-    } else {
-        alert(`You must enter number whithin the range: 16-100`);
-    }
-    document.getElementById('inputBox').value = null;
+  let resized = getData();
+  if (resized >= 16 && resized <= 100) {
+    container(resized);
+  } else {
+    alert(`You must enter number whithin the range: 16-100`);
+  }
+  document.getElementById("inputBox").value = null;
 }
 
 //button for applying black colors
-let blackButton = document.createElement('button');
-blackButton.setAttribute('class', 'color black');
-blackButton.addEventListener('click', function() {
-    applyColor('black');
+let blackButton = document.createElement("button");
+blackButton.setAttribute("class", "color black");
+blackButton.addEventListener("click", function () {
+  applyColor("black");
 });
-blackButton.textContent = 'Black';
+blackButton.textContent = "Black";
 buttonsDiv.appendChild(blackButton);
 
 //Blue Button
-let blueButton = document.createElement('button');
-blueButton.setAttribute('class', 'color blue');
-blueButton.addEventListener('click', function(){
-    applyColor('blue');
-})
-blueButton.textContent = 'Blue';
+let blueButton = document.createElement("button");
+blueButton.setAttribute("class", "color blue");
+blueButton.addEventListener("click", function () {
+  applyColor("blue");
+});
+blueButton.textContent = "Blue";
 buttonsDiv.appendChild(blueButton);
 
 //Red Button
-let redButton = document.createElement('button');
-redButton.setAttribute('class', 'color red');
-redButton.addEventListener('click', function(){
-    applyColor('red');
-})
-redButton.textContent = 'Red';
+let redButton = document.createElement("button");
+redButton.setAttribute("class", "color red");
+redButton.addEventListener("click", function () {
+  applyColor("red");
+});
+redButton.textContent = "Red";
 buttonsDiv.appendChild(redButton);
 
 //Random Color
-let randomButton = document.createElement('button');
-randomButton.setAttribute('class', 'color random');
-randomButton.addEventListener('click', function(){
-    applyColor(`rgb(${Math.floor(Math.random() *255)},${Math.floor(Math.random() *255)} , ${Math.floor(Math.random() *255)})`);
-})
-randomButton.textContent = 'Random';
+let randomButton = document.createElement("button");
+randomButton.setAttribute("class", "color random");
+randomButton.addEventListener("click", function () {
+  applyColor(
+    `rgb(${Math.floor(Math.random() * 255)},${Math.floor(
+      Math.random() * 255
+    )} , ${Math.floor(Math.random() * 255)})`
+  );
+});
+randomButton.textContent = "Random";
 buttonsDiv.appendChild(randomButton);
 
-//color picker 
-let colorPicker = document.createElement('input');
-colorPicker.setAttribute('type', 'color');
-colorPicker.setAttribute('class', 'color picker');
-colorPicker.setAttribute('id', 'colorPickker');
-colorPicker.setAttribute('value', '#008000');
-colorPicker.addEventListener('input', function(){
-    const selected = this.value;
-    applyColor(selected);
-})
-buttonsDiv.appendChild(colorPicker)
+//color picker
+let colorPicker = document.createElement("input");
+colorPicker.setAttribute("type", "color");
+colorPicker.setAttribute("class", "color picker");
+colorPicker.setAttribute("id", "colorPickker");
+colorPicker.setAttribute("value", "#008000");
+colorPicker.addEventListener("input", function () {
+  const selected = this.value;
+  applyColor(selected);
+});
+buttonsDiv.appendChild(colorPicker);
 
-//clear button 
-let clearButton = document.createElement('button');
-clearButton.setAttribute('class', 'color clear ');
-clearButton.setAttribute('type', 'button');
-clearButton.addEventListener('click', function(){
-    applyColor('white')
+//clear button
+let clearButton = document.createElement("button");
+clearButton.setAttribute("class", "color clear ");
+clearButton.setAttribute("type", "button");
+clearButton.addEventListener("click", function () {
+  applyColor("white");
 });
 clearButton.textContent = "Clear";
 buttonsDiv.appendChild(clearButton);
 
-//reset button 
-let resetBtn = document.createElement('button');
-let clearDiv = document.querySelectorAll('.columns');
-resetBtn.addEventListener('click', function(){
-    clearDiv.forEach( function (column){
-        column.style.backgroundColor = 'white';
-    })
+//reset button
+let resetBtn = document.createElement("button");
+let clearDiv = document.querySelectorAll(".columns");
+resetBtn.addEventListener("click", function () {
+  clearDiv.forEach(function (column) {
+    column.style.backgroundColor = "white";
+  });
 });
-resetBtn.setAttribute('class', 'color reset');
+resetBtn.setAttribute("class", "color reset");
 resetBtn.textContent = "Reset";
 buttonsDiv.appendChild(resetBtn);
 
@@ -143,7 +156,8 @@ function applyColor(choice) {
   newColor = choice;
 }
 // function to apply certain colors
-function drawColor() {
-    this.style.backgroundColor = newColor;
-  
+function drawColor(item) {
+  if (!isclicked) return;
+
+  item.style.backgroundColor = newColor;
 }
